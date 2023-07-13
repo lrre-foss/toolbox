c7a870177133f4bfc4bf3ac266f040d41e50a12d
rjindael <me@rjindael.xyz>
2023-07-12 18:57:21 -0700

    add key generator code

--- Toolbox.KeyGenerator/Program.cs
+++ Toolbox.KeyGenerator/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Toolbox.KeyGenerator
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Toolbox.KeyGenerator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int size = 1024;
+            string path = Directory.GetCurrentDirectory();
+
+            if (args.Length > 0 && Directory.Exists(args[0]))
+            {
+                path = args[0];
+            }
+
+            if (args.Length > 1 && int.TryParse(args[1], out int result))
+            {
+                size = result;
+            }
+
+            using RSACryptoServiceProvider rsa = new(size);
+
+            byte[] publicKeyBlob = rsa.ExportCspBlob(false);
+            byte[] privateKeyBlob = rsa.ExportCspBlob(true);
+
+            string publicKeyPem = rsa.ExportRSAPublicKeyPem();
+            string privateKeyPem = rsa.ExportRSAPrivateKeyPem();
+
+            File.WriteAllBytes(Path.Combine(path, "public.bin"), Encoding.UTF8.GetBytes(Convert.ToBase64String(publicKeyBlob)));
+            File.WriteAllBytes(Path.Combine(path, "private.bin"), Encoding.UTF8.GetBytes(Convert.ToBase64String(publicKeyBlob)));
+
+            File.WriteAllBytes(Path.Combine(path, "public.pem"), Encoding.UTF8.GetBytes(publicKeyPem));
+            File.WriteAllBytes(Path.Combine(path, "private.pem"), Encoding.UTF8.GetBytes(privateKeyPem));
         }
     }
 }
